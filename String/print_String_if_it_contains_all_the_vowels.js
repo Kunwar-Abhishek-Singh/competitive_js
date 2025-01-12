e4137b29cc2f60809065a88384eda730faf49502
Kunwar-Abhishek-Singh <aksgaya6@gmail.com>
2025-01-12 22:25:37 +0530

    Print string if it contains all the vowels created

---
+++ String/print_String_if_it_contains_all_the_vowels.js
@@ -0,0 +1,8 @@
+function findString (string) {
+    if(string.includes("a" && "e" && "i" && "o" && "u")){
+        console.log("it has all vowels")
+    }else{
+        console.log("It has not all the vowels");
+    }
+}
+findString("education")
