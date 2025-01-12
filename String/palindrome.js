dac757cdb8efc060dd38dd22c1c1cae052f4adeb
Kunwar-Abhishek-Singh <aksgaya6@gmail.com>
2025-01-12 21:46:42 +0530

    find whether given string is Palindrome or not added

--- String/palindrome.js
+++ String/palindrome.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+function checkPalindrome (string) {
+    let temp="";
+    for(i=string.length-1;i>=0;i--){
+        temp +=string[i]
+    }
+    if(temp === string){
+        console.log("Yes, ",string," is a Palindrome String");
+    }else{
+        console.log("No, ", string, " is not a Palindrome String");
+    }
+}
+
+checkPalindrome("abhishek");
